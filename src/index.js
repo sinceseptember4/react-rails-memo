ac43ced855e668532cd73355d3d71aa7340d6090
sinceseptember4 <sinceseptember4@gmail.com>
2021-12-07 18:16:01 +0900

    index

--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-     <Router basename={process.env.PUBLIC_URL}>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
